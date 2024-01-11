b9ae8f8eb31d6822bd5611775ff4ad8246b0adf6
Bilal Habib <bilal.habib@ba.com>
2024-01-11 16:06:21 +0000

    feat: created profile page tabs

--- src/components/Profile/ProfileTabs.tsx
+++ src/components/Profile/ProfileTabs.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export const ProfileTabs = () => {
-  return (
-    <div>ProfileTabs</div>
-  )
-}
+	return (
+		<Flex
+			w={"full"}
+			justifyContent={"center"}
+			gap={{ base: 4, sm: 10 }}
+			textTransform={"uppercase"}
+			fontWeight={"bold"}
+		>
+			<Flex borderTop={"1px solid white"} alignItems={"center"} p='3' gap={1} cursor={"pointer"}>
+				<Box fontSize={20}>
+          {/* grid */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+          </svg>
+
+				</Box>
+				<Text fontSize={12} display={{ base: "none", sm: "block" }}>
+					Posts
+				</Text>
+			</Flex>
+
+			<Flex alignItems={"center"} p='3' gap={1} cursor={"pointer"}>
+				<Box fontSize={20}>
+          {/* bookmark */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+        </svg>
+
+				</Box>
+				<Text fontSize={12} display={{ base: "none", sm: "block" }}>
+					Saved
+				</Text>
+			</Flex>
+
+			<Flex alignItems={"center"} p='3' gap={1} cursor={"pointer"}>
+				<Box fontSize={20}>
+          {/* likes/heart */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+        </svg>
+				</Box>
+				<Text fontSize={12} display={{ base: "none", sm: "block" }}>
+					Likes
+				</Text>
+			</Flex>
+		</Flex>
+	);
+};
