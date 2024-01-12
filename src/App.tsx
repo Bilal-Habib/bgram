16634669b3fdbc42f9534771037f4c3647b967d2
Bilal Habib <bilal.habib@ba.com>
2024-01-12 15:32:41 +0000

    feat: inlined app routes in App.tsx to handle app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { routes } from "./components/AppRoutes";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import useAuthStore from "./store/authStore";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Profile } from "./pages/Profile";
+import { Signup } from "./pages/Signup";
+import { UploadPost } from "./pages/UploadPost";
 
 function App() {
+  const authUser = useAuthStore(state => state.user)
+
   return (
     <BrowserRouter>
       <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to='/'/>} />
+        <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to='/'/>} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to='/login'/>} />
+        <Route path="/upload-post" element={authUser ? <UploadPost /> : <Navigate to='/login'/>} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to='/login'/>} />
+    </Routes>
     </BrowserRouter>
   );
 }
