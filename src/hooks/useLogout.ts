c5716156d31c1e83b7d2cb39082f50aa9e694785
Bilal Habib <bilal.habib@ba.com>
2024-01-12 15:31:28 +0000

    feat: replaced useNavigate with window reload for useLogout hook

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -2,12 +2,10 @@ import React from 'react'
 import { useSignOut } from 'react-firebase-hooks/auth'
 import { auth } from '../firebase/firebase'
 import { useShowToast } from './useShowToast'
-import { useNavigate } from 'react-router-dom'
 
 export const useLogout = () => {
   const [signOut, isLoggingOut, error] = useSignOut(auth)
   const showToast = useShowToast()
-  const navigate = useNavigate()
 
   const displayErrorMessage = (error: unknown) => {
     if (error instanceof Error) {
@@ -20,6 +18,7 @@ export const useLogout = () => {
   function displaySuccessMessage(success: boolean) {
     if (success) {
       showToast('Success', 'Logged out successfully', 'success')
+      window.location.reload();
     }
   }
 
@@ -28,7 +27,6 @@ export const useLogout = () => {
       const success = await signOut()
       localStorage.removeItem('user-info')
       displaySuccessMessage(success)
-      navigate('/')
     } catch (error) {
       displayErrorMessage(error)
     }
