66b957a313f40f56f27782ab68d983d08a6fe845
Bilal Habib <bilal.habib@ba.com>
2024-01-16 10:33:18 +0000

    feat: created hook to get user profile from firebase

---
+++ src/hooks/useGetUserProfileByUsername.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import { useShowToast } from "./useShowToast"
+import { DocumentData, collection, getDocs, query, where } from "firebase/firestore"
+import { firestore } from "../firebase/firebase"
+import { useUserProfileStore } from "../store/userProfileStore"
+
+export const useGetUserProfileByUsername = (username: string) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const showToast = useShowToast()
+  const {userProfile, setUserProfile} = useUserProfileStore()
+
+  useEffect(() => {
+    const getUserProfile = async () => {
+      setIsLoading(true)
+      try {
+        const usersRef = collection(firestore, 'users')
+        const q = query(usersRef, where('username', '==', username))
+        const querySnapshot = await getDocs(q)
+
+        if (querySnapshot.empty) {
+          setUserProfile(null)
+          return
+        }
+
+        let userDoc: DocumentData | null = null
+        querySnapshot.forEach((doc) => {
+          userDoc = doc.data()
+        })
+
+        setUserProfile(userDoc)
+      } catch (error) {
+        displayErrorMessage(error, showToast)
+      }
+    }
+
+    getUserProfile()
+  }, [setUserProfile, username, showToast])
+
+  return {isLoading, userProfile}
+}
+
+function displayErrorMessage(error: unknown, showToast: (title: string, description: string, status: "info" | "warning" | "success" | "error" | "loading" | undefined) => void) {
+  if (error instanceof Error) {
+    showToast('Error', error.message, 'error')
+  } else {
+    showToast('Error', 'An error occurred with retreiving user profile by username', 'error')
+    console.error(error)
+  }
+}
+
