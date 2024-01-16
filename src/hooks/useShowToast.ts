db3aaff72b93da9bc95145df6a9e6b2cdb729d84
Bilal Habib <bilal.habib@ba.com>
2024-01-16 12:06:34 +0000

    feat: added useCallBack to show toast hook to prevent infinite loop

--- src/hooks/useShowToast.ts
+++ src/hooks/useShowToast.ts
@@ -1,18 +1,22 @@
 import { useToast } from '@chakra-ui/react'
-import React from 'react'
+import { useCallback } from 'react'
 
 export const useShowToast = () => {
     const toast = useToast()
 
-    const showToast = (title: string, description: string, status: "info" | "warning" | "success" | "error" | "loading" | undefined) => {
-        toast({
-            title: title,
-            description: description,
-            status: status,
-            duration: 3000,
-            isClosable: true,
-        })
-    }
+    // useCallback is used to prevent infinite loop, by  caching the function
+	const showToast = useCallback(
+		(title: string, description: string, status: "info" | "warning" | "success" | "error" | "loading" | undefined) => {
+			toast({
+				title: title,
+				description: description,
+				status: status,
+				duration: 3000,
+				isClosable: true,
+			});
+		},
+		[toast]
+	);
 
     return showToast
 }
