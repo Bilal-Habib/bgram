165d7c078b1a99f4033228a230f64e83f1462908
Bilal Habib <bilal.habib@ba.com>
2024-01-17 16:53:18 +0000

    feat: created search user profile hook and add modal to search page

---
+++ src/hooks/useSearchUser.ts
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { useShowToast } from "./useShowToast"
+import { collection, query, where, getDocs, DocumentData } from "firebase/firestore"
+import { firestore } from "../firebase/firebase"
+import { UserDocument } from "../firebase/documentTypes"
+
+export const useSearchUser = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = useState<UserDocument | null>(null)
+  const showToast = useShowToast()
+
+  const getUserProfile = async (username:string) => {
+    setIsLoading(true)
+    try {
+      const usersRef = collection(firestore, 'users')
+      const q = query(usersRef, where('username', '==', username))
+      const querySnapshot: DocumentData = await getDocs(q)
+
+      if (querySnapshot.empty) {
+        return showToast('Error', 'User not found', 'error')
+      }
+
+      querySnapshot.forEach((doc: DocumentData) => {
+        setUser(doc.data())
+      })
+    } catch (error) {
+      displayErrorMessage(error, showToast)
+      setUser(null)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return {isLoading, user, getUserProfile}
+}
+
+function displayErrorMessage(error: unknown, showToast: (title: string, description: string, status: "info" | "warning" | "success" | "error" | "loading" | undefined) => void) {
+  if (error instanceof Error) {
+    showToast('Error', error.message, 'error')
+  } else {
+    showToast('Error', 'An error occurred with retreiving user profile by username', 'error')
+    console.error(error)
+  }
+}
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,23 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Tooltip, useDisclosure } from "@chakra-ui/react";
 import { NavFooter } from "../components/NavBar/NavFooter";
+import { useSearchUser } from "../hooks/useSearchUser";
+import { useRef } from "react";
 
 export const Search = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const {isLoading, user, getUserProfile} = useSearchUser()
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const handleSearchUser = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (searchRef.current) {
+      getUserProfile(searchRef.current.value);
+    }
+  }
+
+  // remove this once done
+  console.log(user);
+
 	return (
 		<>
 			<Tooltip
@@ -20,6 +36,7 @@ export const Search = () => {
 					p={2}
 					w={{ base: 10, md: "full" }}
 					justifyContent={{ base: "center", md: "flex-start" }}
+          onClick={onOpen}
 				>
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -27,6 +44,28 @@ export const Search = () => {
 					<Box display={{ base: "none", md: "block" }}>Search</Box>
 				</Flex>
 			</Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset='slideInLeft'>
+				<ModalOverlay />
+				<ModalContent bg={"white"} border={"1px solid gray"} maxW={"400px"}>
+					<ModalHeader>Search user</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody pb={6}>
+						<form onSubmit={handleSearchUser}>
+							<FormControl>
+								<FormLabel>Username</FormLabel>
+								<Input placeholder='e.g. bilalhabib' ref={searchRef} />
+							</FormControl>
+
+							<Flex w={"full"} justifyContent={"flex-end"}>
+								<Button type='submit' ml={"auto"} size={"sm"} my={4} isLoading={isLoading}>
+									Search
+								</Button>
+							</Flex>
+						</form>
+						{/* {user && <SuggestedUser user={user} setUser={setUser} />} */}
+					</ModalBody>
+				</ModalContent>
+			</Modal>
       <NavFooter />
 		</>
 	);
